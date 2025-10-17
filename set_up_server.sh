bfa070a6b29b6313c113d0751b19d05da1d71dab
jcarlevaro <joana.carlevaro@unibe.ch>
2025-10-17 10:06:40 +0200

    update server specs used

--- set_up_server.sh
+++ set_up_server.sh
@@ -12,10 +12,12 @@ cd ~/AWS-docker
 -p 9001 \
 -a 52.28.165.72
 
-# There are 17 accounts, I will use a "c5a.12xlarge" with 48 cpus and 96G
+# There are 17 accounts, I will use a "c5a.12xlarge" with 48 cpus and 96G or "r5.8xlarge" which is larger.
+# At the end use: r5.8xlarge (took very long for hmmratac, up to 1h.30) 
 ./run_rstudio_server \
 -i jcarlevaro/atacseq_course:v2 \
 -u ./credentials/input_docker_start.txt \
 -p thepass \
 -m 5g \
 -c 2
+
